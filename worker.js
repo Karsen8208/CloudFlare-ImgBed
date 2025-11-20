b8f4ab0b1a20f9818cab3abbe6537145898588e8
Karsen8208 <zttcarse8208@126.com>
2025-11-20 20:48:50 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,5 +1,22 @@
 export default {
     async fetch(request, env) {
-        return new Response("Hello world")
+        const html = `
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>我的 Cloudflare Worker</title>
+        </head>
+        <body>
+            <h1>你好，世界！</h1>
+            <p>这是一个由 Cloudflare Worker 提供的页面。</p>
+        </body>
+        </html>
+        `;
+
+        return new Response(html, {
+            headers: {
+                'Content-Type': 'text/html; charset=utf-8',
+            },
+        });
     }
 }
